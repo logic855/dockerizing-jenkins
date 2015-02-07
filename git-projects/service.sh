27318277576be735f0090c27f67df5e306da5ce2
Frederic Lemay <fredericlemay@Frederics-MacBook-Pro.local>
2015-02-08 10:23:38 +1100

    fixed some issues

--- git-projects/service.sh
+++ git-projects/service.sh
@@ -5,7 +5,7 @@ ACTION=$1
 
 if [ "$1" == "clone" ]
 then
-  echo "cloning ${PROJECTS_REPO}"
+  echo "Clone ${PROJECTS_REPO}"
   git clone ${PROJECTS_REPO}
 elif [ "${ACTION}" == "push" ]
 then
@@ -14,10 +14,10 @@ then
     echo "ERROR push needs a commit message"
     exit 1
   fi
-  echo "push"
+  echo "Add changes, commit and push to ${PROJECTS_REPO}"
   cd dockerized-jenkins-projects
   git add .
-  git commit -m ${2}
+  git commit -m "${2}"
   git push origin master
 else
   echo "USAGE: $0 [clone|push COMMIT_MSG]"
