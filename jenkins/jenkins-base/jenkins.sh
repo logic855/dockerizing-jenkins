bb6a59c3225e6cb1ac1e9adfec6a91511a1f016c
Frederic Lemay <fredericlemay@Frederics-MacBook-Pro.local>
2015-02-10 08:57:51 +1100

    merged plugins and jenkins into a single image

--- jenkins/jenkins-base/jenkins.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec java -jar /opt/jenkins/jenkins.war
--- setup.sh
+++ setup.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
-cat bootmsg.txt
-PLUGINS_IMAGE_TAG="flemay/jenkins-plugins"
-PLUGINS_CONTAINER_NAME="jenkins-plugins"
 
 PROJECTS_IMAGE_TAG="flemay/jenkins-projects"
 PROJECTS_CONTAINER_NAME="jenkins-projects"
 
-JENKINS_BASE_IMAGE_TAG="flemay/jenkins-base"
-JENKINS_IMAGE_TAG="flemay/jenkins:1.597"
+JENKINS_IMAGE_TAG="flemay/jenkins"
 JENKINS_CONTAINER_NAME="jenkins"
 
 echo "Delete all containers"
 for i in $(docker ps -a |awk '!/^C/ {print $1}'); do docker rm -f $i; done
 
-echo "Build ${PLUGINS_IMAGE_TAG} image"
-docker build -t ${PLUGINS_IMAGE_TAG} jenkins-plugins
-
 echo "Build ${PROJECTS_IMAGE_TAG} image"
 docker build -t ${PROJECTS_IMAGE_TAG} jenkins-projects
 
-echo "Build ${JENKINS_BASE_IMAGE_TAG} image"
-docker build -t ${JENKINS_BASE_IMAGE_TAG} jenkins/jenkins-base
-
 echo "Build ${JENKINS_IMAGE_TAG} image"
-docker build -t ${JENKINS_IMAGE_TAG} jenkins/jenkins-1.597
-
-echo "Create ${PLUGINS_CONTAINER_NAME} based on ${PLUGINS_IMAGE_TAG}"
-docker run --name ${PLUGINS_CONTAINER_NAME} ${PLUGINS_IMAGE_TAG} ls /opt/plugins
+docker build -t ${JENKINS_IMAGE_TAG} jenkins
 
 echo "Create ${PROJECTS_CONTAINER_NAME} based on ${PROJECTS_IMAGE_TAG}"
 docker run --name ${PROJECTS_CONTAINER_NAME} ${PROJECTS_IMAGE_TAG} ls /opt/projects
@@ -39,12 +26,8 @@ docker run \
   --name ${JENKINS_CONTAINER_NAME} \
   --privileged \
   -d \
-  --volumes-from ${PLUGINS_CONTAINER_NAME} \
   --volumes-from ${PROJECTS_CONTAINER_NAME} \
   ${JENKINS_IMAGE_TAG}
-
-echo "Wait 20 seconds to let Jenkins boots"
-sleep 20
 
 echo "Open Jenkins in a browser"
 echo open http://$(boot2docker ip 2>/dev/null):8080
