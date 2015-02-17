cf1c93fbea3c989ce814d00fe14725d86a9fc925
Frederic Lemay <fredericlemay@FMBP.local>
2015-02-17 22:42:53 +1100

    slowly moving to ansible to setup my environment

--- ansible.sh
+++ ansible.sh
@@ -3,6 +3,9 @@
 ANSIBLE_IMAGE_TAG="flemay/ansible"
 ANSIBLE_CONTAINER_NAME="ansible"
 
+echo "Delete all containers"
+for i in $(docker ps -a |awk '!/^C/ {print $1}'); do docker rm -f $i; done
+
 echo "Build ${ANSIBLE_IMAGE_TAG} image"
 docker build -t ${ANSIBLE_IMAGE_TAG} ansible
 
@@ -12,8 +15,8 @@ echo "Run a container with flemay/ansible image"
 docker run \
   -it \
   --name ${ANSIBLE_CONTAINER_NAME} \
-  -v $(pwd)/playbooks:/opt/ansible/playbooks \
-  -v $(echo $DOCKER_CERT_PATH):/root/.docker \
+  -v $(pwd)/playbooks:/opt/ansible/playbooks:ro \
+  -v $(echo $DOCKER_CERT_PATH):/root/.docker:ro \
   -e DOCKER_HOST=tcp://$(boot2docker ip):2376 \
   -e DOCKER_TLS_VERIFY=1 \
   ${ANSIBLE_IMAGE_TAG} bash
