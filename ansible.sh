b4ec4805ff4d0af52181cbad17ce519899aaa0d9
Frederic Lemay <fredericlemay@FMBP.local>
2015-02-21 22:32:00 +1100

    separated ansible and docker

--- ansible.sh
+++ ansible.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-ANSIBLE_IMAGE_TAG="flemay/ansible"
-ANSIBLE_CONTAINER_NAME="ansible"
-
 echo "Delete all containers"
 for i in $(docker ps -a |awk '!/^C/ {print $1}'); do docker rm -f $i; done
 
-echo "Build ${ANSIBLE_IMAGE_TAG} image"
-docker build -t ${ANSIBLE_IMAGE_TAG} images/ansible
+echo "Build docker image"
+docker build -t flemay/docker images/docker
 
-docker rm -f ${ANSIBLE_CONTAINER_NAME}
+echo "Build ansible image"
+docker build -t flemay/ansible images/ansible
+
+docker rm -f ansible
 
 echo "Run a container with flemay/ansible image"
 docker run \
   -it \
-  --name ${ANSIBLE_CONTAINER_NAME} \
+  --name ansible \
   -v $(pwd)/playbooks:/opt/ansible/playbooks:ro \
   -v $(pwd)/images:/opt/ansible/images:ro \
   -v $(echo $DOCKER_CERT_PATH):/root/.docker:ro \
   -e DOCKER_HOST=tcp://$(boot2docker ip):2376 \
   -e DOCKER_TLS_VERIFY=1 \
-  ${ANSIBLE_IMAGE_TAG} bash
+  flemay/ansible bash
 
