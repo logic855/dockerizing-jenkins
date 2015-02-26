e34956b3b6d7f046f8d6c1d2cbbe5db2d6ffbe93
Frederic Lemay <fredericlemay@FMBP.local>
2015-02-26 13:11:42 +1100

    renamed clone/push to download/upload

--- images/git-projects/service.sh
+++ images/git-projects/service.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 PROJECTS_REPO=git@github.com:flemay/dockerized-jenkins-projects.git
 
-function clone {
-  ansible-playbook -v /opt/playbooks/clone.yml
+function download {
+  ansible-playbook -v /opt/playbooks/download.yml
 }
 
-function push {
+function upload {
   msg=$1
   if [ "${msg}" == "" ]
     then
     echo "ERROR push needs a commit message"
     exit 1
   fi
-  ansible-playbook -v /opt/playbooks/push.yml --extra-vars "commit_msg='$msg'"
+  ansible-playbook -v /opt/playbooks/upload.yml --extra-vars "commit_msg='$msg'"
 }
 
 command=$1
 case "$command" in
-  clone)
-    clone
+  download)
+    download
     ;;
-  push)
-    push "$2"
+  upload)
+    upload "$2"
     ;;
   *)
-    echo $"Usage: $0 {clone|push} [<args>]"
+    echo $"Usage: $0 {download|upload} [<args>]"
     exit 1
 esac
 
