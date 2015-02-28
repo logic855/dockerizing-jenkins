224452872e427ab55a4945a2f325b2802862090a
Frederic Lemay <fredericlemay@FMBP.local>
2015-02-28 11:33:47 +1100

    moved creation of jobs symlink and copy of global config to jenkins.sh script which makes total sense as the data volume is shared only when creating the container and not during the image creation

--- images/jenkins/jenkins.sh
+++ images/jenkins/jenkins.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echo "make symbol link to use projects"
+ln -s ${PROJECTS_PATH}/jobs $JENKINS_HOME/jobs
+
+echo "copy config file from ${PROJECTS_PATH}"
+cp -f ${PROJECTS_PATH}/config/config.xml $JENKINS_HOME/
+
 exec java -jar /opt/jenkins/jenkins.war
